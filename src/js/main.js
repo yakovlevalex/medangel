42af4108069cb9924b6d257c79f5cf6cae67a950
aleksanderyakovlev <vrn.yakovlev.alex@gmail.com>
2017-06-09 12:26:19 +0300

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js
+
+
+/*
+ * Custom
+ */
+//= partials/app.js
